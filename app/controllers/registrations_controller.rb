8a4430007fb4c96d149e663cd9e1aa4b53ad2bf1
Clement Matheron <clement.matheron@gmail.com>
2015-08-17 22:12:47 +0200

    Add image uploader

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :picture, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :picture, :email, :password, :password_confirmation, :current_password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :picture, PictureUploader
   validates :name, presence: :true
+  validate  :picture_size
 
+
+	private
+
+	    # Validates the size of an uploaded picture.
+	    def picture_size
+	      if picture.size > 5.megabytes
+	        errors.add(:picture, "should be less than 5MB")
+	      end
+	    end
 end
