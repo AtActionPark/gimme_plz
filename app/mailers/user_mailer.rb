33b48d5bd78c9b20306098f75b0811705a0e5ba9
Clement Matheron <clement.matheron@gmail.com>
2015-08-21 17:27:56 +0200

    Add mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "clement.matheron@gmail.com"
-
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcom to Gimme $ Plz')
-  end
-end
